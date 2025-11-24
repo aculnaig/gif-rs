1500075d98b71ada3565b3afc26efa37b266b645
Gianluca Cannata <gcannata23@gmail.com>
2025-11-24 09:33:20 +0100

    Add more GIF constructs
        - DisposalMethod: how to draw the next frame after the previous one
    
        - GraphicControl: optional, preceed an image. Metadata for the
          animation
    
        - ImageDescriptor: 0x2C, defines the rectangle area of a frame

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct LogicalScreenDescriptor {
     pub width: u16,
@@ -34,3 +36,57 @@ pub struct Color {
 }
 
 pub type Palette = Vec<Color>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub enum DisposalMethod {
+    #[default]
+    NoAction = 0,
+    DoNotDispose = 1,
+    RestoreBackground = 2,
+    RestorePrevious = 3,
+    Reserved = 4,
+}
+
+impl From<u8> for DisposalMethod {
+    fn from(n: u8) -> Self {
+        match n {
+            0 => DisposalMethod::NoAction,
+            1 => DisposalMethod::DoNotDispose,
+            2 => DisposalMethod::RestoreBackground,
+            3 => DisposalMethod::RestorePrevious,
+            _ => DisposalMethod::Reserved,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct GraphicControl {
+    pub disposal_method: DisposalMethod,
+    pub user_input_flag: bool,
+    pub transparent_color_index: Option<u8>,
+    pub delay_time_cs: u16, // 1/100 seconds unit
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ImageDescriptor {
+    pub left: u16,
+    pub top: u16,
+    pub width: u16,
+    pub height: u16,
+    pub packed: u8,
+}
+
+impl ImageDescriptor {
+    pub fn has_local_palette(&self) -> bool {
+        (self.packed & 0b1000_0000) != 0
+    }
+
+    pub fn is_interlaced(&self) -> bool {
+        (self.packed & 0b0100_0000) != 0
+    }
+
+    pub fn local_palette_size(&self) -> usize {
+        let n = self.packed & 0b0000_0111;
+        1 << (n + 1)
+    }
+}
